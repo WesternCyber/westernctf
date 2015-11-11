a7b73cd68b37019342b12a23129259ca0951d24e
Harrison Chow <iamnobodyrandom@yahoo.com>
2015-11-11 01:37:21 -0500

    test mailer

--- mail/test-mailer.php
+++ mail/test-mailer.php
@@ -13,19 +13,23 @@ require '../vendor/autoload.php';
 
 $sendTo = "iamnobodyrandom@yahoo.com";
 $sendFrom = "info@westerncyber.club";
+$sendFromName = "Western Cyber Security Club";
 $sendGridUsr = "app43353028@heroku.com";
 $sendGridPassword = "khpoaec44366";
 
 $emailBody = "Test message";
 $emailSubject = "Test email";
+$sendGridTemplateId = "daf01737-4403-4218-92cc-0b888254d80c";
 
 $sendgrid = new SendGrid($sendGridUsr, $sendGridPassword);
 
 $message = new SendGrid\Email();
 $message->addTo($sendTo)->
-setFrom($sendFrom)->
-setSubject($emailSubject)->
-setHtml($emailBody);
+    setFrom($sendFrom)->
+    setFromName($sendFromName)->
+    setSubject($emailSubject)->
+    setHtml($emailBody)->
+    setTemplateId($templateId);
 
 try {
     $sendgrid->send($message);
