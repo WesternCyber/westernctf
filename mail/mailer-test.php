bf61e7859423025739f7b001e61600fb224ced41
Harrison Chow <iamnobodyrandom@yahoo.com>
2015-11-11 00:29:50 -0500

    test mailer

--- mail/mailer-test.php
+++ mail/mailer-test.php
@@ -6,4 +6,24 @@
  * Time: 12:23 AM
  */
 
+$sendTo = "iamnobodyrandom@yahoo.com";
+$sendFrom = "info@westerncyber.club";
+$sendGridUsr = "app43353028@heroku.com";
+$sendGridPassword = "khpoaec44366";
+
+$emailBody = "Test message";
+$emailSubject = "Test email";
+
+require 'vendor/autoload.php';
+$sendgrid = new SendGrid($sendGridUsr, $sendGridPassword);
+
+$message = new SendGrid\Email();
+$message->addTo($sendTo)->
+setFrom($sendFrom)->
+setSubject($emailSubject)->
+//setText($emailBody)->
+setHtml($emailBody);
+$response = $sendgrid->send($message);
+
+echo $response;
 ?>
