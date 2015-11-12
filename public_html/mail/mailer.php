eeb8f287b56791ae784798eea409ce87d3950f6d
Harrison Chow <iamnobodyrandom@yahoo.com>
2015-11-12 18:42:01 -0500

    Try create mailer form

--- public_html/mail/mailer.php
+++ public_html/mail/mailer.php
@@ -8,19 +8,20 @@
 $getPost = (array) json_decode(file_get_contents('php://input'));
 require '../../vendor/autoload.php';
 
-//$sendTo = "iamnobodyrandom@yahoo.com";
 $sendTo = $getPost["email"];
 $sendToName = $getPost["fullName"];
 $sendFrom = "info@westerncyber.club";
 $sendFromName = "Western Cyber Security Club";
 $sendGridUsr = "app43353028@heroku.com";
 $sendGridPassword = "khpoaec44366";
+$sendGridApi = "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";
 
 $emailBody = "Test message";
 $emailSubject = "Test email";
 $sendGridTemplateId = "daf01737-4403-4218-92cc-0b888254d80c";
 
-$sendgrid = new SendGrid($sendGridUsr, $sendGridPassword);
+//$sendgrid = new SendGrid($sendGridUsr, $sendGridPassword);
+$sendgrid = new SendGrid($sendGridApi);
 
 $message = new SendGrid\Email();
 $message
