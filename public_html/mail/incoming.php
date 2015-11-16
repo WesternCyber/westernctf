c816bc6fa43d998c18eda3892a467c3c0cbccfdd
Harrison Chow <iamnobodyrandom@yahoo.com>
2015-11-15 23:55:22 -0500

    Incoming webmail server

--- public_html/mail/incoming.php
+++ public_html/mail/incoming.php
@@ -9,8 +9,10 @@
 $getPost = (array) json_decode(file_get_contents('php://input'));
 //require '../../vendor/autoload.php';
 
+$forwardTo = "iamnobodyrandom@yahoo.com";
+
 $STDERR = fopen('php://stderr', 'w+');
-fwrite($STDERR, $getPost."\n");
+fwrite($STDERR, $getPost['subject']."\n");
 
 echo "OK";
 ?>
