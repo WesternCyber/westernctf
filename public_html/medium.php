1891cb09dd8834cbf087e4b65563d5dcbd8dc366
Harrison Chow <iamnobodyrandom@yahoo.com>
2015-12-23 00:03:08 -0500

    Start medium widget

---
+++ public_html/medium.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: harrisonchow
+ * Date: 12/23/15
+ * Time: 12:01 AM
+ */
+?>
+<div class="widget" >
+<h6 class="title"> Recent Posts </h6>
+<hr>
+<ul class="link-list recent-posts">
+    <li>
+        <a href="https://medium.com/@WesternCyberSecurity/who-we-are-167eb8e56b4a#.ve7qgw9ac"> Who
+            we are </a>
+                                            <span class="date"> November
+                                              <span class="number"> 18, 2015 </span>
+                                          </span>
+    </li>
+</ul>
+</div >
