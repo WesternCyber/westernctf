90f6986e1a97f53374aa49fb38f800339a4aa261
Harrison Chow <iamnobodyrandom@yahoo.com>
2015-12-23 00:06:25 -0500

    Medium widget

--- public_html/medium.php
+++ public_html/medium.php
@@ -8,6 +8,18 @@
 
 echo "<div class=\"widget\">";
 
+$myXMLData =
+    "<?xml version='1.0' encoding='UTF-8'?>
+<note>
+<to>Tove</to>
+<from>Jani</from>
+<heading>Reminder</heading>
+<body>Don't forget me this weekend!</body>
+</note>";
+
+$xml=simplexml_load_string($myXMLData) or die("Error: Cannot create object");
+print_r($xml);
+
 echo "</div>";
 ?>
 <!--<div class="widget">
