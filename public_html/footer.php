c1cda5e9205a58efd2ef09cfbb314e31c32e39bc
Harrison Chow <iamnobodyrandom@yahoo.com>
2015-12-27 00:32:00 -0700

    Optimize code

--- public_html/footer.php
+++ public_html/footer.php
@@ -166,7 +166,7 @@
 </div> <!-- main container close -->
 <script src="js/jquery.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
-<script src="js/flickr.js"></script>
+<script src="js/flickr.min.js"></script>
 <script src="js/flexslider.min.js"></script>
 <script src="js/lightbox.min.js"></script>
 <script src="js/masonry.min.js"></script>
@@ -175,7 +175,7 @@
 <script src="js/ytplayer.min.js"></script>
 <script src="js/countdown.min.js"></script>
 <script src="js/smooth-scroll.min.js"></script>
-<script src="js/parallax2.js"></script>
+<script src="js/parallax2.min.js"></script>
 <script src="js/md5.js"></script>
 <script src="js/scripts.min.js"></script>
 </body>
