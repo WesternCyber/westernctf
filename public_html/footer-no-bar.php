93546f8c8a32453ba5ebcd9b72fca53e942d2260
Anwar Jeffrey <jeffrey.anwar@yahoo.ca>
2016-01-23 02:42:37 -0500

    adding pages

---
+++ public_html/footer-no-bar.php
@@ -0,0 +1,16 @@
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/flickr.min.js"></script>
+<script src="js/flexslider.min.js"></script>
+<script src="js/lightbox.min.js"></script>
+<script src="js/masonry.min.js"></script>
+<script src="js/twitterfetcher.min.js"></script>
+<script src="js/spectragram.min.js"></script>
+<script src="js/ytplayer.min.js"></script>
+<script src="js/countdown.min.js"></script>
+<script src="js/smooth-scroll.min.js"></script>
+<script src="js/parallax2.min.js"></script>
+<script src="js/md5.js"></script>
+<script src="js/scripts.min.js"></script>
+</body>
+</html>
