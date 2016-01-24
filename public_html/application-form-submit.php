ff6653477d96e85c191298d355791019cf6a903d
Anwar Jeffrey <jeffrey.anwar@yahoo.ca>
2016-01-23 19:07:20 -0500

    form page fix and application-form.php set-up

--- public_html/application-form-submit.php
+++ public_html/application-form-submit.php
@@ -1,5 +1,12 @@
 <?php
 echo "FORM DATA START\n";
 $email = $_POST["email"];
+$name = $_POST["name"]
+$number = $_POST["number"]
+$resume = $_POST["fileToUpload"]
+$schoolyear = $_POST["school-year"]
+$program = $_POST["program"]
+$linkedin = $_POST["linkedin"]
+$github = _$_POST["github"]
 echo "FORM DATA END\n";
 ?>
