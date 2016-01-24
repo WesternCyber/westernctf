fbbd1e1e5785033a56df32538eccdd9c2a82536a
Harrison Chow <hchow52@uwo.ca>
2016-01-23 20:09:57 -0500

    Fix typo

--- public_html/application-form-submit.php
+++ public_html/application-form-submit.php
@@ -9,4 +9,10 @@ $program = $_POST["program"];
 $linkedin = $_POST["linkedin"];
 $github = $_POST["github"];
 echo "FORM DATA END\n";
+
+require 'vendor/autoload.php';
+use Parse\ParseClient;
+ParseClient::initialize('1YK2gxEAAxFHBHR4DjQ6yQOJocIrtZNYjYwnxFGN', 'Ceoe5j4JTYsxR5EvQaX1vdmGNWKXSRfQWx4GLLso', 'RYf7WRp7HGEvyDX6geKVe3HPjpslqGGR6SbkEBDf');
+
+
 ?>
