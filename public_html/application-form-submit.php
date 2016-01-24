43b7531cd84cabbfe073325e0b033f1275772bb3
Harrison Chow <hchow52@uwo.ca>
2016-01-24 00:40:54 -0500

    Fix typo

--- public_html/application-form-submit.php
+++ public_html/application-form-submit.php
@@ -1,8 +1,4 @@
 <?php
-require '../vendor/autoload.php';
-use Parse\ParseClient;
-ParseClient::initialize('SeHk9pU9MA4vuoCGc5knx0VKsy8PoOPJu5ZrxLna', 'ln8Pq8b9VD47rvXE80C5UIyE5btckohZN6RDzTad', 's2who59fnh6PYBmlcJrC9W1ND8aj71fPqvITey4p');
-
 echo "FORM DATA START<br>";
 $position = $_POST["position"];
 $email = $_POST["email"];
@@ -15,6 +11,10 @@ $linkedin = $_POST["linkedin"];
 $github = $_POST["github"];
 echo $position . "<br>" . $email . "<br>" . $name . "<br>" . $number. "<br>" . "<br>" . $schoolyear . "<br>" . $program. "<br>" . $linkedin. "<br>" . $github;
 echo "FORM DATA END<br>";
+
+require '../vendor/autoload.php';
+use Parse\ParseClient;
+ParseClient::initialize('SeHk9pU9MA4vuoCGc5knx0VKsy8PoOPJu5ZrxLna', 'ln8Pq8b9VD47rvXE80C5UIyE5btckohZN6RDzTad', 's2who59fnh6PYBmlcJrC9W1ND8aj71fPqvITey4p');
 
 $application = new ParseObject("applications");
 
