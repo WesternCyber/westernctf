6f23eb9d5fe258a2eab95823008882304a881230
Anwar Jeffrey <jeffrey.anwar@yahoo.ca>
2016-01-23 21:56:41 -0500

    form page fix and application-form.php set-up

--- public_html/application-form-submit.php
+++ public_html/application-form-submit.php
@@ -8,11 +8,22 @@ $schoolyear = $_POST["school-year"];
 $program = $_POST["program"];
 $linkedin = $_POST["linkedin"];
 $github = $_POST["github"];
-echo $email.$name.$number.$resume.$schoolyear.$program.$linkedin.$github;
+echo $email . "\n" . $name . "\n" . $number. "\n" . $resume. "\n" . $schoolyear . "\n" . $program. "\n" . $linkedin. "\n" . $github;
 echo "FORM DATA END\n";
 
 require 'vendor/autoload.php';
 use Parse\ParseClient;
 ParseClient::initialize('1YK2gxEAAxFHBHR4DjQ6yQOJocIrtZNYjYwnxFGN', 'Ceoe5j4JTYsxR5EvQaX1vdmGNWKXSRfQWx4GLLso', 'RYf7WRp7HGEvyDX6geKVe3HPjpslqGGR6SbkEBDf');
+
+$application = new ParseObject("application");
+
+$application->set("name", $name);
+$application->set("email", $email);
+$application->set("number", $number);
+$application->set("resume", $resume);
+$application->set("schoolyear", $schoolyear);
+$application->set("program", $program);
+$application->set("linkedin", $linkedin);
+$application->set("github", $github);
 
 ?>
