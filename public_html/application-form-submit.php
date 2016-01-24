cd4944faa0cdd19d9517ea38215c4a47608989b6
Harrison Chow <hchow52@uwo.ca>
2016-01-24 00:47:41 -0500

    Fix typo

--- public_html/application-form-submit.php
+++ public_html/application-form-submit.php
@@ -17,7 +17,7 @@ use Parse\ParseClient;
 ParseClient::initialize('SeHk9pU9MA4vuoCGc5knx0VKsy8PoOPJu5ZrxLna', 'ln8Pq8b9VD47rvXE80C5UIyE5btckohZN6RDzTad', 's2who59fnh6PYBmlcJrC9W1ND8aj71fPqvITey4p');
 echo "abc<br>";
 $application = new ParseObject("applications");
-
+echo "lol<br>";
 $application->set("name", $name);
 $application->set("email", $email);
 $application->set("number", $number);
@@ -28,12 +28,12 @@ $application->set("linkedin", $linkedin);
 $application->set("github", $github);
 
 try {
-  $application->save();
-  echo 'New object created with objectId: ' . $application->getObjectId();
-} catch (ParseException $ex) {  
-  // Execute any logic that should take place if the save fails.
-  // error is a ParseException object with an error code and message.
-  echo 'Failed to create new object, with error message: ' . $ex->getMessage();
+    $application->save();
+    echo 'New object created with objectId: ' . $application->getObjectId();
+} catch (ParseException $ex) {
+// Execute any logic that should take place if the save fails.
+// error is a ParseException object with an error code and message.
+    echo 'Failed to create new object, with error message: ' . $ex->getMessage();
 }
 
 ?>
