27acce4e2833adf93e6fb4fed73a4fdaad8c88c7
Harrison Chow <hchow52@uwo.ca>
2016-01-26 20:45:50 -0500

    Fix typo

--- public_html/application-form-submit.php
+++ public_html/application-form-submit.php
@@ -10,16 +10,14 @@ $program = $_POST["program"];
 $linkedin = $_POST["linkedin"];
 $github = $_POST["github"];
 echo $position . "<br>" . $email . "<br>" . $name . "<br>" . $number. "<br>" . "<br>" . $schoolyear . "<br>" . $program. "<br>" . $linkedin. "<br>" . $github;
-echo "FORM DATA END<br>";
+echo "<br>FORM DATA END<br>";
 
 require '../vendor/autoload.php';
 use Parse\ParseClient;
 use Parse\ParseObject;
 ParseClient::initialize('SeHk9pU9MA4vuoCGc5knx0VKsy8PoOPJu5ZrxLna', 'ln8Pq8b9VD47rvXE80C5UIyE5btckohZN6RDzTad', 's2who59fnh6PYBmlcJrC9W1ND8aj71fPqvITey4p');
 
-echo "abc<br>";
 $application = new ParseObject("applications");
-echo "lol<br>";
 $application->set("name", $name);
 $application->set("email", $email);
 $application->set("number", $number);
@@ -37,6 +35,8 @@ try {
 // error is a ParseException object with an error code and message.
     echo 'Failed to create new object, with error message: ' . $ex->getMessage();
 }
+
+echo "<br>PARSE DONE";
 
 echo "<br>START PROCESS IMAGE<br>";
 $target_dir = "uploads/";
