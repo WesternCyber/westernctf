5254565362f840c8e3de7070a55f520a134e464f
Harrison Chow <hchow52@uwo.ca>
2016-01-26 20:37:42 -0500

    Add file processing - Testing phase

--- public_html/application-form-submit.php
+++ public_html/application-form-submit.php
@@ -38,4 +38,22 @@ try {
     echo 'Failed to create new object, with error message: ' . $ex->getMessage();
 }
 
+echo "<br>START PROCESS IMAGE<br>";
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
+echo $target_file;
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+    $check = filesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+}
+echo "<br>DONE PROCESSING IMAGE";
 ?>
