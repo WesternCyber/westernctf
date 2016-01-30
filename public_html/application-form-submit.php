8982aa272408d0d82e7a40c66fdc86ff8af68441
Harrison Chow <hchow52@uwo.ca>
2016-01-30 17:09:12 -0500

    Change output to JSON

--- public_html/application-form-submit.php
+++ public_html/application-form-submit.php
@@ -1,5 +1,6 @@
 <?php
-echo "FORM DATA START<br>";
+$errMsg = "";
+$success = true;
 $position = $_POST["position"];
 $email = $_POST["email"];
 $name = $_POST["name"];
@@ -9,8 +10,11 @@ $schoolyear = $_POST["school-year"];
 $program = $_POST["program"];
 $linkedin = $_POST["linkedin"];
 $github = $_POST["github"];
-echo $position . "<br>" . $email . "<br>" . $name . "<br>" . $number. "<br>" . "<br>" . $schoolyear . "<br>" . $program. "<br>" . $linkedin. "<br>" . $github;
-echo "<br>FORM DATA END<br>";
+
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
 
 require '../vendor/autoload.php';
 use Parse\ParseClient;
@@ -21,7 +25,7 @@ $application = new ParseObject("applications");
 $application->set("name", $name);
 $application->set("email", $email);
 $application->set("number", $number);
-//$application->set("resume", $resume);
+$application->set("resume", $target_file);
 $application->set("schoolyear", $schoolyear);
 $application->set("program", $program);
 $application->set("linkedin", $linkedin);
@@ -29,21 +33,15 @@ $application->set("github", $github);
 
 try {
     $application->save();
-    echo 'New object created with objectId: ' . $application->getObjectId();
+    //echo 'New object created with objectId: ' . $application->getObjectId();
 } catch (ParseException $ex) {
 // Execute any logic that should take place if the save fails.
 // error is a ParseException object with an error code and message.
-    echo 'Failed to create new object, with error message: ' . $ex->getMessage();
+    //echo 'Failed to create new object, with error message: ' . $ex->getMessage();
+    $success = false;
+    $errMsg = $errMsg . 'Failed to create new object, with error message: ' . $ex->getMessage() . '<br>';
 }
 
-echo "<br>PARSE DONE";
-
-echo "<br>START PROCESS FILE<br>";
-$target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
-echo $target_file;
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
     /*$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
@@ -58,12 +56,12 @@ if(isset($_POST["submit"])) {
 
 // Check if file already exists
 if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
+    $errMsg = $errMsg . "Sorry, file already exists.<br>";
     $uploadOk = 0;
 }
 // Check file size
 if ($_FILES["fileToUpload"]["size"] > 500000) {
-    echo "Sorry, your file is too large.";
+    $errMsg = $errMsg . "Sorry, your file is too large.<br>";
     $uploadOk = 0;
 }
 
@@ -76,16 +74,20 @@ if ($_FILES["fileToUpload"]["size"] > 500000) {
 
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
+    $errMsg = $errMsg . "Sorry, your file was not uploaded.<br>";
+    $success = false;
 // if everything is ok, try to upload file
 } else {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+        $errMsg = $errMsg . "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.<br>";
     } else {
-        echo "Sorry, there was an error uploading your file.";
+        $errMsg = $errMsg . "Sorry, there was an error uploading your file.";
+        $success = false;
     }
 }
-echo "<br>DONE PROCESSING FILE";
+
+// Message out JSON
+echo "{success:" . $success . ",errorMessage:" . $errMsg . "}";
 
 header("Location: http://westerncyber.club/submitted"); /* Redirect browser */
 exit();
