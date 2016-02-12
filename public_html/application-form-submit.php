b336a4c1082f15a4b4174df5797679e098ad3ee6
Harrison Chow <hchow52@uwo.ca>
2016-02-12 02:05:45 -0500

    Try fix form

--- public_html/application-form-submit.php
+++ public_html/application-form-submit.php
@@ -19,6 +19,8 @@ $imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
 require '../vendor/autoload.php';
 use Parse\ParseClient;
 use Parse\ParseObject;
+use Parse\ParseFile;
+
 ParseClient::initialize('SeHk9pU9MA4vuoCGc5knx0VKsy8PoOPJu5ZrxLna', 'ln8Pq8b9VD47rvXE80C5UIyE5btckohZN6RDzTad', 's2who59fnh6PYBmlcJrC9W1ND8aj71fPqvITey4p');
 $application = new ParseObject("applications");
 $application->set("position", $position);
