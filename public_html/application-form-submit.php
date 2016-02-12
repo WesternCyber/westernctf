c8639cc673a85c4c539a2d4062670ab78750f6a7
Harrison Chow <hchow52@uwo.ca>
2016-02-12 01:31:21 -0500

    Fix

--- public_html/application-form-submit.php
+++ public_html/application-form-submit.php
@@ -20,7 +20,7 @@ require '../vendor/autoload.php';
 use Parse\ParseClient;
 use Parse\ParseObject;
 ParseClient::initialize('SeHk9pU9MA4vuoCGc5knx0VKsy8PoOPJu5ZrxLna', 'ln8Pq8b9VD47rvXE80C5UIyE5btckohZN6RDzTad', 's2who59fnh6PYBmlcJrC9W1ND8aj71fPqvITey4p');
-
+echo $position;
 $application = new ParseObject("applications");
 $application->set("position", $position);
 $application->set("name", $name);
@@ -90,6 +90,6 @@ if ($uploadOk == 0) {
 // Message out JSON
 echo "{\"success\":" . $success . ",\"errorMessage\":\"" . $errMsg . "\"}";
 
-header("Location: http://westerncyber.club/submitted"); /* Redirect browser */
+//header("Location: http://westerncyber.club/submitted"); /* Redirect browser */
 exit();
 ?>
