966eb46d88817663e1f316bf8280b297cd687081
Zilin Xie <zxie43@uwo.ca>
2016-11-10 15:44:22 -0500

    workshop 2: added blank challenges

---
+++ public_html/workshop2.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Zilin
+ * Date: 2016-11-10
+ * Time: 3:30 PM
+ */
+
+include_once("header.html");
+include_once("workshop2questions.html");
+include_once("footer.php");
+?>
