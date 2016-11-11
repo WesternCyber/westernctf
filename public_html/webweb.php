081681bc61fca5f770c7404cbcaf4b2337450702
Zilin Xie <zxie43@uwo.ca>
2016-11-11 02:50:39 -0500

    added web q

--- public_html/webweb.php
+++ public_html/webweb.php
@@ -1,5 +1,5 @@
 <?php
   //set headers to NOT cache a page
-header("flag-is-here: flag{hahahaha}"); //HTTP 1.1
+header("flag-is-here: ZmxhZ3tjYW5fdV9mZWVsX3RoZV9CRVJOfQ=="); //HTTP 1.1
 include_once("webweb.html");
 ?>
